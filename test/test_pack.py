2f5be25fe85eb84c229345845e888626735400e7
Jack <jack.grahl@yahoo.co.uk>
2016-09-06 22:57:20 +0100

    Move creating a pack into setUp().

--- test/test_pack.py
+++ test/test_pack.py
@@ -3,10 +3,11 @@ import unittest
 from pack import Pack
 
 class TestPack(unittest.TestCase):
+    def setUp(self):
+        self.pack = Pack()
+
     def test_get_card(self):
-        pack = Pack()
-        self.assertIsNotNone(pack.get_card())
+        self.assertIsNotNone(self.pack.get_card())
 
     def test_get_card_twice(self):
-        pack = Pack()
-        self.assertNotEqual(pack.get_card(), pack.get_card())
+        self.assertNotEqual(self.pack.get_card(), self.pack.get_card())
