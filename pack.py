be979d8b4eba3415ee67ce5f63b0fccceb13338d
Jack <jack.grahl@yahoo.co.uk>
2016-09-06 22:52:07 +0100

    Properly implement drawing a card from the pack.

--- pack.py
+++ pack.py
@@ -1,3 +1,5 @@
+import random
+
 from deuces import Card
 
 NUMBERS = '23456789TJQKA'
@@ -5,5 +7,9 @@ SUITS = 'cdhs'
 WHOLE_PACK = set([ Card.new(number + suit) for number in NUMBERS for suit in SUITS ])
 
 class Pack():
+    cards = WHOLE_PACK.copy()
+
     def get_card(self):
-        return Card.new('Ah')
+        card = random.choice(list(self.cards))
+        self.cards.remove(card)
+        return card
--- test/test_pack.py
+++ test/test_pack.py
@@ -6,3 +6,7 @@ class TestPack(unittest.TestCase):
     def test_get_card(self):
         pack = Pack()
         self.assertIsNotNone(pack.get_card())
+
+    def test_get_card_twice(self):
+        pack = Pack()
+        self.assertNotEqual(pack.get_card(), pack.get_card())
