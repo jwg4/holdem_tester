ceec97aa06aa82a1930536130b77ff17d24d190f
Jack <jack.grahl@yahoo.co.uk>
2016-09-06 23:03:17 +0100

    Shuffle pack.

--- pack.py
+++ pack.py
@@ -7,9 +7,13 @@ SUITS = 'cdhs'
 WHOLE_PACK = set([ Card.new(number + suit) for number in NUMBERS for suit in SUITS ])
 
 class Pack():
-    cards = WHOLE_PACK.copy()
+    def __init__(self):
+        self.shuffle()
 
     def get_card(self):
         card = random.choice(list(self.cards))
         self.cards.remove(card)
         return card
+
+    def shuffle(self):
+        self.cards = WHOLE_PACK.copy()
--- test/test_pack.py
+++ test/test_pack.py
@@ -11,3 +11,9 @@ class TestPack(unittest.TestCase):
 
     def test_get_card_twice(self):
         self.assertNotEqual(self.pack.get_card(), self.pack.get_card())
+
+    def test_shuffle(self):
+        card1 = self.pack.get_card()
+        self.pack.shuffle()
+        for i in range(0, 52):
+            self.assertIsNotNone(self.pack.get_card())
